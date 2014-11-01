bd9ca5c1d4b93aaf2cb58c0706c199498e33af38
Eddie Fang <eddie.fang.email@gmail.com>
2014-11-01 16:09:17 -0700

    removed not needed code, only simple example rest needed

--- jersey2-spring4-grizzly2-master 2/src/main/java/com/test/resources/TestResource.java
+++ jersey2-spring4-grizzly2-master 2/src/main/java/com/test/resources/TestResource.java
@@ -1,16 +1,10 @@
 package com.test.resources;
 
-import java.security.SecureRandom;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import com.test.domain.User;
-import com.test.domain.interfaces.UserManager;
 
 /**
  * TestResource
@@ -20,31 +14,10 @@ import com.test.domain.interfaces.UserManager;
 @Path("/")
 public class TestResource {
 
-	@Autowired
-	private UserManager userManager;
-
-	@GET
-	@Produces("text/html")
-	public String getIt() {
-		User user = new User();
-		user.setId(new SecureRandom().nextInt());
-		user.setName("test");
-		user.setUsername("username");
-
-		this.userManager.insertUserAndFailTransation(user);
-		return "uups";
-	}
-
 	@GET
 	@Path("/test")
 	@Produces("text/html")
 	public String test() {
-		User user = new User();
-		user.setId(new SecureRandom().nextInt());
-		user.setName("test");
-		user.setUsername("username");
-
-		this.userManager.insertUser(user);
 		return "test";
 	}
 
